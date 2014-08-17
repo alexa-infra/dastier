9fd69e24e0a5973701f17250e9ee726fef2a1ef4
Alexey Vasilyev <alexa.infra@gmail.com>
2014-08-17 12:26:54 +0200

    simple api

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from database import db_session
 from bookmarks import bookmarks_page
 from readitlater import readitlater_page
+from flask.ext.restless import APIManager
+from model import Link, ReadLater
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -14,6 +16,12 @@ app.config.update(dict(
 
 app.register_blueprint(bookmarks_page)
 app.register_blueprint(readitlater_page, url_prefix='/readitlater')
+
+manager = APIManager(app, session=db_session)
+manager.create_api(Link, methods=['GET', 'POST', 'PUT', 'DELETE'],
+                   url_prefix='/api', collection_name='bookmark')
+manager.create_api(ReadLater, methods=['GET', 'POST', 'PUT', 'DELETE'],
+                   url_prefix='/api', collection_name='readitlater')
 
 @app.teardown_appcontext
 def shutdown(error):
