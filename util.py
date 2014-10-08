b895600d08f769ce946181ef17fad7ad86e82abb
Alexey Vasilyev <alexa.infra@gmail.com>
2014-10-08 07:04:49 +0200

    minor fixes

--- util.py
+++ util.py
@@ -6,5 +6,5 @@ def truncate_char_to_space(data, num):
     if data.find(' ', num, num + 5) == -1:
         return data[:num] + '...'
     else:
-        return data[:num] + data[data:data.find(' ', num)] + '...'
+        return data[:data.find(' ', num)] + '...'
 
